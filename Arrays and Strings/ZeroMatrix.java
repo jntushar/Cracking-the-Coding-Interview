e7505747093d69b23f7ec5d38fb9dc1285c734d0
Tushar Jain <tjain4736@gmail.com>
2021-12-05 17:44:13 +0530

    Question 1.8 - fix bug

--- Arrays and Strings/ZeroMatrix.java
+++ Arrays and Strings/ZeroMatrix.java
@@ -16,34 +16,40 @@ public class ZeroMatrix {
 		print(getZeroMatrix(matrix));
 	}
 	
-	static int[][] getZeroMatrix(int[][] matrix){
-		int row=0,col=0;
-		for(int i=0; i<matrix.length; i++) {
-			for(int j=0; j<matrix[i].length; j++) {
-				if(matrix[i][j] == 0) {
-					row = i;
-					col = j;
-					break;
-				}
-			}
-		}
-		
-		//to nullify row
-		for(int x=0; x<matrix[row].length; x++)
-			matrix[row][x] = 0;
-			
-		//to nullify column
-		for(int x=0; x<matrix.length; x++)
-			matrix[x][col] = 0;
-		return matrix;
-	}
+	private static int[][] getZeroMatrix(int[][] matrix){
+        boolean[] row = new boolean[matrix.length];
+        boolean[] column = new boolean[matrix[0].length];
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                if(matrix[i][j] == 0) {
+                    row[i] = true;
+                    column[j] = true;
+                }
+            }
+        }
+        for(int i=0; i<row.length; i++){
+            if(row[i]){
+                for(int m=0; m<matrix[0].length; m++){
+                    matrix[i][m] = 0;
+                }
+            }
+        }
+        for(int j=0; j<column.length; j++){
+            if(column[j]){
+                for(int n=0; n<matrix.length; n++){
+                    matrix[n][j] = 0;
+                }
+            }
+        }
+        return matrix;
+    }
 	
-	static void print(int[][] array) {
-		for(int i=0; i<array.length; i++) {
-			for(int j=0; j<array[i].length; j++) { 
-				System.out.print(array[i][j] + " ");
-			}
-			System.out.println();
-		}
-	}
+    private static void print(int[][] array) {
+        for(int i=0; i<array.length; i++) {
+            for(int j=0; j<array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
